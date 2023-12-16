56e06c132058f9d4c86e3846f0a8ee65f579c721
vivek_bajpai <bajpai8765vivek@gmail.com>
2023-12-16 13:45:05 +0000

    some changes in ui

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,7 +1,8 @@
 import 'package:flutter/material.dart';
-
+import 'package:snippet_coder_utils/hex_color.dart';
+import 'package:snippet_coder_utils/ProgressHUD.dart';
 import '../services/auth_services.dart';
-//import '../screens/home_screen.dart';
+import '../screens/home_screen.dart';
 
 class LoginScreen extends StatefulWidget {
   const LoginScreen({Key? key}) : super(key: key);
@@ -11,54 +12,91 @@ class LoginScreen extends StatefulWidget {
 }
 
 class _LoginScreenState extends State<LoginScreen> {
+  bool isAPIcallProcess = false;
+  bool hidePassword = false;
+  GlobalKey<FormState> globalFormKey = GlobalKey<FormState>();
+  String? username;
+  String? password;
+
   final AuthService authService =
       AuthService(baseUrl: 'https://products-9q0g.onrender.com');
   final TextEditingController usernameController = TextEditingController();
   final TextEditingController passwordController = TextEditingController();
 
-  Future<void> login() async {
+  bool isLoading = false;
+
+  Future<dynamic> login() async {
+    setState(() {
+      isLoading = true;
+    });
+
     final String username = usernameController.text;
     final String password = passwordController.text;
 
     final Map<String, dynamic> result =
         await authService.login(username, password);
 
+    setState(() {
+      isLoading = true;
+    });
+
     if (result.containsKey('error')) {
       print('Login error: ${result['error']}');
     } else {
-      // Login successful, handle success (e.g., navigate to another screen)
       print('Login successful');
       print('User Profile: ${result['username']}, ${result['email']}');
+
+      if (context.mounted) {
+        return Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => const HomeScreen(),
+          ),
+        );
+      }
     }
   }
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        title: const Text('Login Page'),
-      ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            TextField(
-              controller: usernameController,
-              decoration: const InputDecoration(labelText: 'Username'),
+    return SafeArea(
+      child: Scaffold(
+          backgroundColor: HexColor("#283B71"),
+          body: ProgressHUD(
+            child: Form(
+              key: globalFormKey,
+              child: _loginUI(context),
             ),
-            TextField(
-              controller: passwordController,
-              obscureText: true,
-              decoration: const InputDecoration(labelText: 'Password'),
-            ),
-            const SizedBox(height: 20),
-            ElevatedButton(
-              onPressed: login,
-              child: const Text('Login'),
-            ),
-          ],
-        ),
+            inAsyncCall: isAPIcallProcess,
+            key: UniqueKey(),
+          )),
+    );
+  }
+
+  Widget _loginUI(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.start,
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Container(
+            width: MediaQuery.of(context).size.width,
+            height: MediaQuery.of(context).size.height / 5,
+            decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  begin: Alignment.topCenter,
+                  end: Alignment.bottomCenter,
+                  colors: [
+                    Colors.white,
+                    Colors.white,
+                  ],
+                ),
+                borderRadius: BorderRadius.only(
+                  bottomLeft: Radius.circular(100),
+                  bottomRight: Radius.circular(100),
+                )),
+          )
+        ],
       ),
     );
   }
