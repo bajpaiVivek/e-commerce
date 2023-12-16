522cb7eee695283ec8983ff8edcd0c0ccc0f2270
vivek_bajpai <bajpai8765vivek@gmail.com>
2023-12-16 18:59:46 +0000

    ui changes

--- lib/screens/login_screen.dart
+++ lib/screens/login_screen.dart
@@ -1,4 +1,6 @@
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:snippet_coder_utils/FormHelper.dart';
 import 'package:snippet_coder_utils/hex_color.dart';
 import 'package:snippet_coder_utils/ProgressHUD.dart';
 import '../services/auth_services.dart';
@@ -63,12 +65,12 @@ class _LoginScreenState extends State<LoginScreen> {
       child: Scaffold(
           backgroundColor: HexColor("#283B71"),
           body: ProgressHUD(
+            inAsyncCall: isAPIcallProcess,
+            key: UniqueKey(),
             child: Form(
               key: globalFormKey,
               child: _loginUI(context),
             ),
-            inAsyncCall: isAPIcallProcess,
-            key: UniqueKey(),
           )),
     );
   }
@@ -100,11 +102,111 @@ class _LoginScreenState extends State<LoginScreen> {
               children: [
                 Align(
                   alignment: Alignment.center,
-                  child: Image.asset("assets/images/ecommerce.png"),
+                  child: Image.asset(
+                    "images/ecommerce.png",
+                    width: 250,
+                    fit: BoxFit.contain,
+                  ),
                 )
               ],
             ),
-          )
+          ),
+          Padding(
+            padding: const EdgeInsets.only(left: 20, bottom: 30, top: 50),
+            child: Text(
+              'Login',
+              style: TextStyle(
+                fontWeight: FontWeight.bold,
+                fontSize: 25,
+                color: Colors.white,
+              ),
+            ),
+          ),
+          FormHelper.inputFieldWidget(
+            context,
+            "username",
+            "Username",
+            (onValidateVal) {
+              if (onValidateVal.isEmpty) {
+                return "username can\t be empty.";
+              }
+              return null;
+            },
+            (onSavedVal) {
+              username = onSavedVal;
+            },
+            prefixIcon: const Icon(Icons.person),
+            borderFocusColor: Colors.white,
+            prefixIconColor: Colors.white,
+            borderColor: Colors.white,
+            textColor: Colors.white,
+            hintColor: Colors.white.withOpacity(.7),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 10),
+            child: FormHelper.inputFieldWidget(context, "password", "Password",
+                (onValidateVal) {
+              if (onValidateVal.isEmpty) {
+                return "password can\t be empty.";
+              }
+              return null;
+            }, (onSavedVal) {
+              password = onSavedVal;
+            },
+                prefixIcon: const Icon(Icons.lock),
+                borderFocusColor: Colors.white,
+                prefixIconColor: Colors.white,
+                borderColor: Colors.white,
+                textColor: Colors.white,
+                hintColor: Colors.white.withOpacity(.7),
+                obscureText: hidePassword,
+                suffixIcon: IconButton(
+                    onPressed: () {
+                      setState(() {
+                        hidePassword = !hidePassword;
+                      });
+                    },
+                    color: Colors.white.withOpacity(.7),
+                    icon: Icon(hidePassword
+                        ? Icons.visibility_off
+                        : Icons.visibility))),
+          ),
+          Align(
+            alignment: Alignment.bottomRight,
+            child: Padding(
+              padding: const EdgeInsets.only(right: 25, top: 10),
+              child: RichText(
+                text: TextSpan(
+                    style: const TextStyle(
+                      color: Colors.grey,
+                      fontSize: 14.0,
+                    ),
+                    children: <TextSpan>[
+                      TextSpan(
+                          text: 'Forget Password ?',
+                          style: TextStyle(
+                              color: Colors.white,
+                              decoration: TextDecoration.underline),
+                          recognizer: TapGestureRecognizer
+                            ..onTap = () {
+                              print('Forget password');
+                            }),
+                    ]),
+              ),
+            ),
+          ),
+          SizedBox(
+            height: 20,
+          ),
+          Center(
+            child: FormHelper.submitButton(
+              "Login",
+              () {},
+              btnColor: HexColor("#283B71"),
+              borderColor: Colors.white,
+              txtColor: Colors.white,
+            ),
+          ),
         ],
       ),
     );
